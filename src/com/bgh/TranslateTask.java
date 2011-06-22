a4d74a67ece0d201c602e1e9e1a10fdf5fdb8bf6
tramchamploo <tramchamploo@gmail.com>
2011-06-22 22:37:23 +0800

    1.0

---
+++ src/com/bgh/TranslateTask.java
@@ -0,0 +1,17 @@
+package com.bgh;
+
+/**
+ * User: asuka
+ * Date: 11-6-22
+ * Time: 下午10:35
+ */
+public class TranslateTask implements Runnable {
+
+    private static final String TAG = "TranslateTask";
+    private final Translate translate;
+    private final String origin, from, to;
+
+    public void run() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
