1c68d716c72d1ca9628e1248ae856cb8060f888d
tramchamploo <tramchamploo@gmail.com>
2011-06-23 21:04:22 +0800

    final release

--- gen/com/bgh/R.java
+++ gen/com/bgh/R.java
@@ -28,13 +28,14 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int back_text=0x7f050005;
-        public static final int empty=0x7f050006;
+        public static final int back_text=0x7f050003;
+        public static final int empty=0x7f050005;
         public static final int from_text=0x7f050001;
-        public static final int original_hint=0x7f050002;
-        public static final int to_text=0x7f050003;
-        public static final int translated_hint=0x7f050004;
-        public static final int translating=0x7f050007;
-        public static final int translation_error=0x7f050008;
+        public static final int original_hint=0x7f050004;
+        public static final int to_text=0x7f050002;
+        public static final int translated_hint=0x7f050009;
+        public static final int translating=0x7f050006;
+        public static final int translation_error=0x7f050007;
+        public static final int translation_interrupted=0x7f050008;
     }
 }
--- src/com/bgh/TranslateTask.java
+++ src/com/bgh/TranslateTask.java
@@ -1,5 +1,16 @@
 package com.bgh;
 
+import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 /**
  * User: asuka
  * Date: 11-6-22
@@ -8,16 +19,67 @@ package com.bgh;
 public class TranslateTask implements Runnable {
     private static final String TAG = "TranslateTask";
     private final Translate translate;
-    private final String origin, from, to;
+    private final String original, from, to;
 
     public TranslateTask(Translate translate, String original, String from, String to) {
         this.translate = translate;
-        this.origin = original;
+        this.original = original;
         this.from = from;
         this.to = to;
     }
 
     public void run() {
+        String trans = doTranslate(original, from, to);
+        translate.setTranslated(trans);
+        String retrans = doTranslate(trans, to, from);
+        translate.setRetranslated(retrans);
+    }
 
+    private String doTranslate(String original, String from, String to) {
+        String result = translate.getResources().getString(R.string.translation_error);
+        HttpURLConnection con = null;
+        Log.d(TAG, "doTranslate(" + original + ", " + from + ", " + to + ")");
+        try {
+            if (Thread.interrupted())
+                throw new InterruptedException();
+
+            String q = URLEncoder.encode(original, "UTF-8");
+            URL url = new URL("http://ajax.googleapis.com/ajax/services/language/translate" +
+                    "?v=1.0&q=" + q + "&langpair=" + from + "%7C" + to);
+            con = (HttpURLConnection) url.openConnection();
+            con.setReadTimeout(10000 /*milliseconds*/);
+            con.setConnectTimeout(15000);
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Referer", "http://www.pragprog.com/titles/eband3/" +
+                    "hello-android");
+            con.setDoInput(true);
+
+            //start the query
+            con.connect();
+            if (Thread.interrupted())
+                throw new InterruptedException();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+            String payload = reader.readLine();
+            reader.close();
+
+            JSONObject jsonObject = new JSONObject(payload);
+            result = jsonObject.getJSONObject("responseData").getString("translatedText")
+                    .replace("&#39;", "'")
+                    .replace("&amp;", "&");
+        } catch (IOException e) {
+            Log.e(TAG, "IOException", e);
+        } catch (JSONException e) {
+            Log.e(TAG, "JSONException", e);
+        } catch (InterruptedException e) {
+            Log.d(TAG, "InterruptedException", e);
+            result = translate.getResources().getString(R.string.translation_interrupted);
+        } finally {
+            if (con != null) con.disconnect();
+        }
+
+        //All done
+        Log.d(TAG, "   -> returned " + result);
+        return result;
     }
 }
